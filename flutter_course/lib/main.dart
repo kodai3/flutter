a34ce821131502e63acf5a64955da665cbf308c0
kodai3 <k3dai.su3@gmail.com>
2018-12-09 00:08:33 +0900

    basic

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -4,15 +4,31 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-         home: Scaffold(
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
           appBar: AppBar(
-            title: Text(
-              'EasyList'
-            ),
+            title: Text('EasyList'),
           ),
-        ),
-      );
-    }
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Product'),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/images.jpg'),
+                    Text('Caho')
+                  ],
+                ),
+              ),
+            ],
+          )),
+    );
+  }
 }
